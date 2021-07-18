6e471b431794d006f4b28b24d8f0264f3083039f
binarycache <binarycache@users.noreply.github.com>
2021-07-18 01:07:59 +0000

    deploy: 89c55b977f00577b244786a0501d3dd953f76db5

---
+++ assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Artificial General Intelligence",
+        "excerpt":"This is a series of blog posts which will explain functional Brain Connectivity i.e. the study of statiscally significant temporal connections in anatomically segregated brain regions which are activated during a task. Don't worry if you didn't get any of the above. I will be explaining everything in a series...","categories": ["Brain Connectivity"],
+        "tags": [],
+        "url": "https://binarycache.github.io/logical_musings/brain%20connectivity/2020/04/21/Artifical-General-Intelligence-The-Prelude-to-Brain-Connectivity.html"
+      },{
+        "title": "Depression and Reward Learning",
+        "excerpt":"According to the APA [1], Depression (major depressive disorder) is a common and serious medical illness that negatively affects how you feel, the way you think and how you act . For a more detailed and comprehensive understanding of the basics of depression, please refer to my post \"What is...","categories": ["Depression"],
+        "tags": [],
+        "url": "https://binarycache.github.io/logical_musings/depression/2020/08/29/Depression-And-Reward-Learning.html"
+      },{
+        "title": "What is Depression?",
+        "excerpt":"                    ","categories": ["Depression"],
+        "tags": [],
+        "url": "https://binarycache.github.io/logical_musings/depression/2020/08/29/Deression.html"
+      }]
